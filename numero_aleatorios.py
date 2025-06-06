32494a247fc7700ed019fe05135e3f2ccea2f26e
Adam-Verdugo <ad.verdugo@duocuc.cl>
2025-06-06 11:04:12 -0400

    version3

--- numero_aleatorios.py
+++ numero_aleatorios.py
@@ -4,9 +4,21 @@ numeros = []
 
 
 
-for x in range(1000):
-    n = random.randint(1,1000)
-    numeros.append(random.randint(1,1000))
+for x in range(10):
+    n = random.randint(1,10)
+    numeros.append(random.randint(1,10))
 
 numeros.sort()
 print(numeros)
+
+par = 0
+impar = 0
+
+for i in range(len(numeros)):
+    if numeros[i]%2==0:
+        par +=1
+    else: impar +=1
+
+print(f"""cantidad de numeros pares : {par}
+cantidad de numeros impars : {impar}""")
+
